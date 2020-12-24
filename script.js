79e4889b587a9163395c4289ba7974f907772529
TokyoProgramming <samuraidjango.jp@gmail.com>
2020-12-24 16:21:14 +0900

    add single meal

--- script.js
+++ script.js
@@ -48,5 +48,80 @@ const searchMeal = (e) => {
     alert('this is empty, enter something in');
   }
 };
+
+// Fetch meal by ID
+const getMealById = async (mealID) => {
+  const res = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`
+  );
+  const data = await res.json();
+
+  const meal = data.meals[0];
+  addMealToDOM(meal);
+};
+
+// Fetch ramdomMeal
+const randomMeal = async () => {
+  // Clear meals and heading
+  mealsEl.innerHTML = '';
+  resultHeading.innerHTML = '';
+
+  const res = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+  const data = await res.json();
+  console.log(data);
+  const meal = data.meals[0];
+
+  addMealToDOM(meal);
+};
+
+// Add meal to DOM
+const addMealToDOM = (meal) => {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+	  <div class="single-meal">
+		<h1>${meal.strMeal}</h1>
+		<img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+		<div class="single-meal-info">
+		  ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+		  ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+		</div>
+		<div class="main">
+		  <p>${meal.strInstructions}</p>
+		  <h2>Ingredients</h2>
+		  <ul>
+			${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+		  </ul>
+		</div>
+	  </div>
+	`;
+};
+
 //Event listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', randomMeal);
+
+mealsEl.addEventListener('click', (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+  console.log(mealInfo);
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealById(mealID);
+  }
+});
